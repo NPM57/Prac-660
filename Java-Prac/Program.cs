5e74d5a305452be85ab404ae3c5baff36f984b5c
Phuong My Nguyen <n9786350@qut.edu.au>
2018-03-01 17:15:23 +1000

    practical 1

--- Java-Prac/Program.cs
+++ Java-Prac/Program.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Java_Prac
+namespace GPLexTutorial
 {
     class Program
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("Hello");
         }
     }
 }
